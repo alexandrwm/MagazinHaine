23579fb857a03a52f6bfe5a8475d87329dbb9ff8
alexandraciobanu <alexandraciobanu@ALEXANDRACI9519>
2024-04-23 10:04:50 +0300

    Add project files.

---
+++ MagazinHaine/Controllers/HomeController.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Web.Mvc;
+
+
+namespace ClothesShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        public HomeController()
+        {
+        }
+        public ActionResult Index()
+        {
+            ViewData["NoContainerClass"] = true;
+            
+
+            ViewBag.ErrorMessage = TempData["ErrorMessage"];
+            return View();
+            //return RedirectToAction("Shop");
+        }
+
+        public ActionResult Privacy()
+        {
+            return View();
+        }
+
+        public ActionResult Error()
+        {
+            return View();
+        }
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string userName, string userPass)
+        {
+
+            //return RedirectToAction("Index");
+            return RedirectToAction("Check", "Cart");
+
+        }
+        public ActionResult Logout()
+        {
+            HttpContext.Session.Clear();
+            return RedirectToAction("Index");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Shop(string stext)
+        {
+                return RedirectToAction("Index");
+        }
+
+        public ActionResult Shop()
+        {
+         
+            return View();
+        }
+        public ActionResult men()
+        {
+
+			return View();
+        }
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public ActionResult men(string stext)
+		{
+			if (stext == null)
+			{
+				return RedirectToAction("men");
+			}
+			
+			return View();
+		}
+
+		public ActionResult women()
+        {
+			
+			return View();
+        }
+
+        public ActionResult kids()
+        {
+			
+			return View();
+        }
+
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+    }
+}
+    
